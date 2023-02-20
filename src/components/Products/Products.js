85f148b3180c7dd1a2856a1275396987e6e2c9a1
Animal <61629960+AnimalStudioOfficial@users.noreply.github.com>
2023-02-20 17:04:50 -0500

    Update Products.js

--- src/components/Products/Products.js
+++ src/components/Products/Products.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
-import ProjectCard from "./ProductsCards";
+import ProductsCard from "./ProductsCards";
 import Particle from "../Particle";
 
 import bayesnet from "../../assets/Projects/bayesnet.jpg";
